1d4a048524b052be8389d5cf8f5ddb54350ebb7d
FontellaH <136127924+FontellaH@users.noreply.github.com>
2023-06-11 13:12:53 -0700

    Html_Dictionary_Enty_Assignment

---
+++ Dictionary_Entry.js
@@ -0,0 +1,16 @@
+<DOCTYPE HTML>
+<html>
+<head>
+    <title> Dictionary Entry</title>
+</head>
+<body>
+    <h1>Dictionary Entry</h1>
+    <p> Ninja</p>
+    <p>'noun'</p>
+        <ol>
+            <li>A practitioner skilled in the Japanese art of ninjutsu</li>
+            <li>A programmer who breaks down problems with zen-like focus</li>
+        </ol>
+</body>
+</html>
+</DOCTYPE>
